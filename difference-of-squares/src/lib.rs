e64e0b8aa4f3610cc815e6db65f196cbe19ac56c
Cody Django Redmond <codydjango@gmail.com>
2019-05-12 21:10:21 -0700

    improved

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,9 +1,15 @@
+// originally had:
+// (1u32..=n).collect::<Vec<u32>>().iter().sum::<u32>().pow(2)
+// simplified to:
 pub fn square_of_sum(n: u32) -> u32 {
-    (1u32..=n).collect::<Vec<u32>>().iter().sum::<u32>().pow(2)
+    (1u32..=n).sum::<u32>().pow(2)
 }
 
+// originally had:
+// (1u32..=n).collect::<Vec<u32>>().iter().map(|i| { i.pow(2) }).sum()
+// simplified to:
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1u32..=n).collect::<Vec<u32>>().iter().map(|i| { i.pow(2) }).sum()
+    (1u32..=n).map(|i| i.pow(2)).sum()
 }
 
 pub fn difference(n: u32) -> u32 {
